56db40d28f71578b650fba45934833198bbf50c7
LJ S.A <lvjhn.d@gmail.com>
2024-12-13 23:35:55 +0800

    general update

--- src/Encoder_.js
+++ src/Encoder_.js
@@ -5,6 +5,7 @@ export default class Encoder_
      * Encodes a float array into bytes.
      */
     static encodeFloatArrayToBytes(floatArray) {
+
         const f32a          = new Float32Array(floatArray)
         const bytes         = new Uint8Array(f32a.buffer)
         return bytes
@@ -14,6 +15,10 @@ export default class Encoder_
      * Decodes a float array from bytes.
      */
     static decodeFloatArrayFromBytes(bytes) {
+        if(!bytes.buffer) {
+            bytes = new Uint8Array(bytes)
+        }
+
         const items_f32     = new Float32Array(
             bytes.buffer, 
             bytes.byteOffset, 
@@ -36,6 +41,10 @@ export default class Encoder_
      * Decodes an integer array from bytes.
      */
     static decodeIntArrayFromBytes(bytes) {
+        if(!bytes.buffer) {
+            bytes = new Uint8Array(bytes)
+        }
+
         const items_i32     = new Int32Array(
             bytes.buffer, 
             bytes.byteOffset, 

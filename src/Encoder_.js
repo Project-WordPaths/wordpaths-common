24a79e3a62027f1cde2391069fd68bf06d124141
LJ S.A <lvjhn.d@gmail.com>
2024-12-13 20:27:28 +0800

    added tests for Encoder_

--- src/Encoder_.js
+++ src/Encoder_.js
@@ -1,31 +1,47 @@
 
-class Encoder_
+export default class Encoder_
 {
     /**
      * Encodes a float array into bytes.
      */
-    static enodeFloatArrayToBytes(floatArray) {
-
+    static encodeFloatArrayToBytes(floatArray) {
+        const f32a          = new Float32Array(floatArray)
+        const bytes         = new Uint8Array(f32a.buffer)
+        return bytes
     }   
 
     /**
      * Decodes a float array from bytes.
      */
-    static decodeFloatArrayFromBytes() {
-
+    static decodeFloatArrayFromBytes(bytes) {
+        const items_f32     = new Float32Array(
+            bytes.buffer, 
+            bytes.byteOffset, 
+            bytes.byteLength / Float32Array.BYTES_PER_ELEMENT
+        );
+        const items = Array.from(items_f32)
+        return items
     }
 
     /**
      * Encodes integer array to bytes.
      */
-    static encodeIntArrayToBytes() {
-
+    static encodeIntArrayToBytes(intArray) {
+        const f32a          = new Int32Array(intArray)
+        const bytes         = new Uint8Array(f32a.buffer)
+        return bytes
     }
 
     /**
      * Decodes an integer array from bytes.
      */
-    static decodeIntegerArrayBytes() {
-
+    static decodeIntArrayFromBytes(bytes) {
+        const items_i32     = new Int32Array(
+            bytes.buffer, 
+            bytes.byteOffset, 
+            bytes.byteLength / Int32Array.BYTES_PER_ELEMENT
+        );
+        const items = Array.from(items_i32)
+        return items
     }
 }
---
+++ test/Encoder_.test.js
@@ -0,0 +1,30 @@
+import assert from "assert";
+import Encoder_ from "wordpaths-common/src/Encoder_.js"
+
+
+describe("Encoder_", () => {
+    describe("Float Array Encoding", () => {
+        it("must encode and decode properly", () => {
+            const originalArray = [1.9, 2.8, 3.7, 4.6, 5.5, 4.6, 3.7, 2.8, 1.9]
+            const bytes = Encoder_.encodeFloatArrayToBytes(originalArray)
+            const derivedArray = 
+                Encoder_
+                    .decodeFloatArrayFromBytes(bytes)
+                    .map(x => parseFloat(x.toFixed(2)))
+            assert.deepEqual(originalArray, derivedArray)
+        })
+    })
+
+
+    describe("Integer Array Encoding", () => {
+        it("must encode and decode properly", () => {
+            const originalArray = [9, 8, 7, 6, 5, 1, 2, 3, 4]
+            const bytes = Encoder_.encodeIntArrayToBytes(originalArray)
+            const derivedArray = 
+                Encoder_
+                    .decodeIntArrayFromBytes(bytes)
+                    .map(x => parseFloat(x.toFixed(2)))
+            assert.deepEqual(originalArray, derivedArray)
+        })
+    })
+})
